4d9954626ecbf8235e5d9d07202fba873bfe35b8
stosoroc <108575040+stosoroc@users.noreply.github.com>
2023-01-15 20:54:38 +0200

    alt joc cu zar
    aici poti introduce cite parti are zar 
    si cite zaruri avem

---
+++ lesson_15_homework/rand2.py
@@ -0,0 +1,22 @@
+# Using the Random Module, create a small program that will simulate 2 x 6 sided dice.
+# The programm should allow the user to roll the dice, and show the values on each dice, and the total sum.
+
+# Improve the exercise above to let the user pick how many sides the Dice has and how many dice there are.
+# After the dice are configured, let the user roll the dice until STOP.
+
+import random
+print()
+parti = int(input("Cite parti are zar: "))
+zaruri = int(input("Cite zaruri avem: "))
+
+while True:
+    total = 0
+    for i in range(zaruri):
+        zar = random.randint(1, parti)
+        total += zar
+        print(f"Zar{i+1}:  " + str(zar))
+    
+    print("Total: " + str(total))
+    inca = input("\nInca o data? (Enter pentru DA): ")
+    if inca:
+        break
