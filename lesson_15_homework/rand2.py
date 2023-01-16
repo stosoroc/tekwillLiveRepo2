f6578b526c0275ac9d9748af79421b0b92ef4b42
stosoroc <108575040+stosoroc@users.noreply.github.com>
2023-01-16 13:30:28 +0200

    import functii din rand.py

--- lesson_15_homework/rand2.py
+++ lesson_15_homework/rand2.py
@@ -5,18 +5,23 @@
 # After the dice are configured, let the user roll the dice until STOP.
 
 import random
+from lesson_15_homework.rand import zar_start
+
 print()
+
 parti = int(input("Cite parti are zar: "))
 zaruri = int(input("Cite zaruri avem: "))
 
-while True:
-    total = 0
-    for i in range(zaruri):
-        zar = random.randint(1, parti)
-        total += zar
-        print(f"Zar{i+1}:  " + str(zar))
+zar_start(parti,zaruri)
+
+# while True:
+#     total = 0
+#     for i in range(zaruri):
+#         zar = random.randint(1, parti)
+#         total += zar
+#         print(f"Zar{i+1}:  " + str(zar))
     
-    print("Total: " + str(total))
-    inca = input("\nInca o data? (Enter pentru DA): ")
-    if inca:
-        break
+#     print("Total: " + str(total))
+#     inca = input("\nInca o data? (Enter pentru DA): ")
+#     if inca:
+#         break
