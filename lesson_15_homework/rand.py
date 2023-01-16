2b236679c6c9c26dd984bb00f0d0467cc238726b
stosoroc <108575040+stosoroc@users.noreply.github.com>
2023-01-16 13:29:47 +0200

    modificari cu functii

--- lesson_15_homework/rand.py
+++ lesson_15_homework/rand.py
@@ -4,15 +4,38 @@
 # and show the values on each dice, and the total sum.
 
 import random
-print()
-while True:
-    zar1 = random.randint(1, 6)
-    zar2 = random.randint(1, 6)
-    total = zar1 + zar2
-    print("Un  Zar: " + str(zar1))
-    print("Alt Zar: " + str(zar2))
-    print("Total:   " + str(total))
-    inca = input("\nInca o data? (Enter pentru DA): ")
-    if inca:
-        break
+
+
+def zar_random(parti,zaruri):
+    total = 0
+    print()
+    for i in range(zaruri):
+        zar = random.randint(1, parti)
+        total += zar
+        print(f"Zar{i+1}:  " + str(zar))
+    print("Total: " + str(total))
+
+def zar_start(parti,zaruri):
+    while True:
+        zar_random(parti,zaruri)
+        inca = input("\nInca o data? (Enter pentru DA): ")
+        if inca:
+            break
+    
+parti = 6
+zaruri = 2
+
+zar_start(parti,zaruri)
+
+# while True:
+#     # zar1 = random.randint(1, 6)
+#     # zar2 = random.randint(1, 6)
+#     # total = zar1 + zar2
+#     # print("Un  Zar: " + str(zar1))
+#     # print("Alt Zar: " + str(zar2))
+#     # print("Total:   " + str(total))
+#     zar_random(parti,zaruri)
+#     inca = input("\nInca o data? (Enter pentru DA): ")
+#     if inca:
+#         break
 
