6a287aaa3de4231dacb57cf854ad3f31dd151810
mtricolici <mtricolici@inthergroup.com>
2023-02-15 17:47:16 +0200

    Final Project updates

--- lesson_24_live/openwheatermap_exmaple.py
+++ lesson_24_live/openwheatermap_exmaple.py
@@ -1,9 +1,21 @@
-from lesson_24_live.find_coord_for_ciry import find_coord_for_city
-
-url = "https://api.openweathermap.org/data/2.5/weather"
+def find_coord_for_city(city):
+    url = "http://api.openweathermap.org/geo/1.0/direct"
+    params = {
+        'q': city,
+        'appid': 'f41891f4e00281944de24eeae3d3efd4',
+    }
+    import requests
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        print(response.json())
+        return dict(lat=response.json()[0]['lat'], lon=response.json()[0]['lon'])
+    else:
+        print('Request failed with status', response.status_code)
+        return None
 
 
 def get_weather(position_info: dict):
+    url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
         **position_info,
         'appid': 'f41891f4e00281944de24eeae3d3efd4',
